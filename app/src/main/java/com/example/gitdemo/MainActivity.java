a6c9d3945706af21064d8ca4bc1bcfe8da503b55
cuiyaya <227380872@qq.com>
2018-11-27 14:59:37 +0800

    111

---
+++ app/src/main/java/com/example/gitdemo/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.gitdemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //完成首页功能
+        //完成分类功能g
+    }
+}
